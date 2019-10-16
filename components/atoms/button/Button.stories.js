20cbcd925ccd12ad29cadd6d5df5ac91dd93bb75
Gauthier <bosson.gauthier27@gmail.com>
2019-10-16 12:21:18 +0200

    Added atomic design structure + storybook

---
+++ components/atoms/button/Button.stories.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import Button from './Button';
+
+export default { title: "Button" };
+
+export const test = () => <Button />;
+
+test.story = {
+  name: 'test button'
+};
